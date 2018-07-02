d71861b1c837e42ba5c42d95cb83e735660c67e9
blankaf <realblankaf@gmail.com>
2018-07-02 22:17:01 +0200

    chiron: Update fingerprint to V9.6.1.0.ODEMIFD
    Change-Id: Ia0c1709a8552ed2708ccfb83d2fb71de016231fe

--- lineage.mk
+++ lineage.mk
@@ -12,6 +12,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="chiron-user 8.0.0 OPR1.170623.027 V9.5.4.0.ODEMIFA release-keys"
+    PRIVATE_BUILD_DESC="chiron-user 8.0.0 OPR1.170623.027 V9.6.1.0.ODEMIFD release-keys"
 
-BUILD_FINGERPRINT := Xiaomi/chiron/chiron:8.0.0/OPR1.170623.027/V9.5.4.0.ODEMIFA:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/chiron/chiron:8.0.0/OPR1.170623.027/V9.6.1.0.ODEMIFD:user/release-keys
